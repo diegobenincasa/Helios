24be7b674f76aba2edac91260f70027cb271e9e4
Diego Benincasa <diego.benincasa@lightbase.com.br>
2025-01-29 12:24:12 -0300

    first commit

---
+++ src/test/java/br/lightbase/helios/HeliosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lightbase.helios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
