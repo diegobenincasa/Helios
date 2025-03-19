2825d20afc04199d7e8bcf2239286ba6deee24e1
Diego Benincasa <diego.benincasa@lightbase.com.br>
2025-03-19 00:16:36 -0300

    [FIX] Correção para deploy na AWS.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting new application..."
-nohup java -jar /home/ec2-user/helios.war > /home/ec2-user/app.log 2>&1 &
+nohup java -jar /home/ec2-user/helios-1.0.war > /home/ec2-user/app.log 2>&1 &
